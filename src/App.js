4518e01e9feffe248341dbc129242f00896b0510
Saurabh-Thakur-07 <saurabhthakurmeerut@gmail.com>
2022-02-02 20:55:46 +0530

    basic ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Todo from './Components/Todo';
 function App() {
   return (
-    <h1>Helloo</h1>
-  );
+          <Todo />
+      );
 }
 
 export default App;
--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 
 export default class Todo extends Component {
-  render() {
-    return <div></div>;
-  }
+    constructor(){
+        super();
+        this.state={
+            currTask:'',
+            tasks:[{task:"read mails",id:1},
+            {task:"go for a run",id:2}
+            ,{task:"do react and attend lect",id:3}]
+        }
+    }
+    render() {
+        return (<div>
+            <input type="text" />
+            <button>Submit</button>
+            <ul>
+                {
+                    this.state.tasks.map((taskObj)=>(
+                        <li key={taskObj.id}>
+                            <p>{taskObj.task}</p>
+                            <button>Delete</button>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>);
+    }
 }
