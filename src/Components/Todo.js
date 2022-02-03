32a6cd1f35b47e63c2370c3d451d94985ae0582e
Saurabh-Thakur-07 <saurabhthakurmeerut@gmail.com>
2022-02-03 11:17:23 +0530

    delete and add functionality added

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -10,16 +10,37 @@ export default class Todo extends Component {
             ,{task:"do react and attend lect",id:3}]
         }
     }
+    handleChange =(e)=>{
+        this.setState({
+                currTask:e.target.value
+        })
+    }
+    handleSubmit =()=>{
+        this.setState({
+            tasks: [...this.state.tasks,{task:this.state.currTask,id:this.state.tasks.length+1}],
+            currTask: ''
+        })
+    }
+    handleDelete =(id)=>{
+        let taskList = this.state.tasks.filter((task)=>{
+            return task.id != id ;
+        })
+        this.setState(
+            {
+                tasks: [...taskList]
+            }
+        )
+    }
     render() {
         return (<div>
-            <input type="text" />
-            <button>Submit</button>
+            <input type="text" onChange={this.handleChange} value={this.state.currTask} />
+            <button onClick={this.handleSubmit}>Submit</button>
             <ul>
                 {
                     this.state.tasks.map((taskObj)=>(
                         <li key={taskObj.id}>
                             <p>{taskObj.task}</p>
-                            <button>Delete</button>
+                            <button onClick={()=>this.handleDelete(taskObj.id)}>Delete</button>
                         </li>
                     ))
                 }
